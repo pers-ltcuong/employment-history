47e636d45b5fdcca3feaa3587d36f012014f5185
pers-ltcuong <tcuong.1204@gmail.com>
2025-05-13 13:34:06 +0700

    Add contact by phone/email/github/linkedin

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import "./Section.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope, faPhone
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faLinkedin,
+  faGithub,
+  faFacebook
+} from "@fortawesome/free-brands-svg-icons";
 
 function Contact() {
   return (
     <div className="container">
       <h2>Contact</h2>
-      <p>Get in touch with me via email or LinkedIn.</p>
+      <p>Get in touch with me via:</p>
+      <div className="icon-links">
+        <a href="tel:+843355144433">
+          <FontAwesomeIcon icon={faPhone} size="2x" />
+        </a>
+        <a href="mailto:ltcuong.work@gmail.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faEnvelope} size="2x" />
+        </a>
+        <a href="https://www.linkedin.com/in/ltcuong98" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+        <a href="https://github.com/pers-ltcuong" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+      </div>
     </div>
   );
 }
