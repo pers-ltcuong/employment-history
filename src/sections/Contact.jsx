67f38696548c37da5d9c83c12896c07ae29f9517
Cuong Le <tcuong.1204@gmail.com>
2025-05-14 23:27:37 +0700

    Add MS logo, remove github

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -7,7 +7,7 @@ import {
 import {
   faLinkedin,
   faGithub,
-  faFacebook
+  faMicrosoft
 } from "@fortawesome/free-brands-svg-icons";
 
 function Contact() {
@@ -22,11 +22,11 @@ function Contact() {
         <a href="mailto:ltcuong.work@gmail.com" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faEnvelope} size="2x" />
         </a>
+        <a href="msteams:/l/chat/0/0?users=tcuong.1204@outlook.com.vn">
+          <FontAwesomeIcon icon={faMicrosoft} size="2x" />
+        </a>
         <a href="https://www.linkedin.com/in/ltcuong98" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faLinkedin} size="2x" />
-        </a>
-        <a href="https://github.com/pers-ltcuong" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
       </div>
     </div>
