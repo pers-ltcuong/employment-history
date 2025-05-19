754c9c3e6a035bf16f25b2e8ca4273b6522f2110
Cuong Le <tcuong.1204@gmail.com>
2025-05-19 20:44:51 +0700

    Improve about me and hero page also, avoid duplicating

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -5,8 +5,19 @@ function About() {
   return (
     <div className="container">
       <h2>About Me</h2>
-      <p>I believe great software starts with great testing. With a focus on clarity, consistency, and teamwork, I aim to ensure every product is reliable, intuitive, and ready for real-world use.</p>
-      <p>Driven by quality and efficiency, I blend test automation with hands-on testing to ensure smooth, stable releases and great user experiences.</p>
+      <p>
+        I've contributed to over 20 successful releases and tested around 250 features across networking and web-based products - from when I started working up to May 19, 2025.
+        These include a wide range of networking, security, and UI features.
+      </p>
+      <p>
+        I specialize in both manual and automated testing, combining detailed bug reporting, robust test script development, and close collaboration with development teams to ensure smooth, stable, and user-friendly releases.
+      </p>
+      <p>
+        I'm always learning, always improving, and always aiming to test smarter.
+      </p>
+      <p>
+        <b>I don't rely on specific tools - I'm quick to adapt and learn new technologies as needed.</b>
+      </p>
     </div>
   );
 }
